60145066150cfe24a8dcb281fd2d3c8b9a2fc98d
Dikshya Limbu <dikshyalimbu@192-168-1-20.tpgi.com.au>
2024-09-21 00:45:31 +1000

    update model logic

--- backend/app/models/vqa_model.py
+++ backend/app/models/vqa_model.py
@@ -1,34 +1,29 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.regularizers import l2
+from transformers import RobertaTokenizer, TFRobertaModel
+from tensorflow.keras.applications import ResNet50
+import keras
 
 # Define the VQAModel class with Keras model
-class VQAModel(tf.keras.Model):
-    def __init__(self, trainable=True, **kwargs):  # Accept 'trainable' and other kwargs
-        super(VQAModel, self).__init__(**kwargs)
-        self.trainable = trainable  # You can choose to use or ignore this
-        self.dense1 = Dense(512, activation='relu', kernel_regularizer=l2(0.01))
-        self.dropout = Dropout(0.5)
-        self.flatten = Flatten()
-        self.dense2 = Dense(2, activation='softmax')
+class VQAModel:
+    def __init__(self, model_path):
+        # Load the pre-trained Keras model
+        self.model = keras.models.load_model(model_path)
+        
+        # Initialize ResNet50 model for image feature extraction
+        self.resnet_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 
-    def call(self, inputs):
-        x = self.dense1(inputs)
-        x = self.dropout(x)
-        x = self.flatten(x)
-        return self.dense2(x)
+        # Load the pre-trained Roberta Tokenizer and model
+        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+        self.roberta_model = TFRobertaModel.from_pretrained('roberta-large')
 
-    def get_config(self):
-        config = super(VQAModel, self).get_config()
-        config.update({"trainable": self.trainable})
-        return config
+    def get_model(self):
+        return self.model
+    
+    def get_resnet(self):
+        return self.resnet_model
 
-# Function to load the Keras model
-def get_model(model_path):
-    model = tf.keras.models.load_model(model_path, custom_objects={'VQAModel': VQAModel})
-    return model
-
-# Function to load the tokenizer
-def get_tokenizer(tokenizer_path='roberta-base'):
-    tokenizer = RobertaTokenizer.from_pretrained(tokenizer_path)
-    return tokenizer
+    def get_tokenizer(self):
+        return self.tokenizer
+    
+    def get_roberta_model(self):
+        return self.roberta_model
