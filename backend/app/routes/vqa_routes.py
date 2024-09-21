8daef8e5b851a74cfaa2d826c18379df592b8e02
Dikshya Limbu <dikshyalimbu@192-168-1-20.tpgi.com.au>
2024-09-21 20:39:08 +1000

    set up logger

--- backend/app/routes/vqa_routes.py
+++ backend/app/routes/vqa_routes.py
@@ -1,10 +1,18 @@
 from flask import Blueprint, request, jsonify
 from backend.app.services.vqa_service import predict_vqa
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 vqa_routes = Blueprint('vqa', __name__)
 
 @vqa_routes.route('/vqa', methods=['POST'])
 def vqa():
+    # verify whether the vqa endpoint is reached
+    logger.info("Test endpoint reached")
+    
     image = request.files.get('image')
     question = request.form.get('question')
 
