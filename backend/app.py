65918e233f04bbce8ebea0492f9c87190987c7af
Dikshya Limbu <dikshyalimbu@192-168-1-20.tpgi.com.au>
2024-10-09 18:17:40 +1100

    enable dynamic port binding for render

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from backend.app.routes.vqa_routes import vqa_routes
 
@@ -15,4 +16,7 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True)
+    
+    # Dynamically bind to the port provided by Render (or default to 5000 for local dev)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
