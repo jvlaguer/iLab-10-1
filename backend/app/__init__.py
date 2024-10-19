d41dbd12caafa0aa34c498133d0aa1eac2df3a02
Dikshya Limbu <dikshyalimbu@Dikshya-MacBook-Air.local>
2024-10-19 14:11:59 +1100

    removed config.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
-from app.config import Config
 from app.routes import vqa_routes
+
 
 def create_app():
     app = Flask(__name__)
-
-    # Load configuration
-    app.config.from_object(Config)
 
     # Register Blueprints (modular routes)
     app.register_blueprint(vqa_routes)
