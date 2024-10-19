4740fce4a46bd2a3b5515d09f24c7000141d560b
Dikshya Limbu <dikshyalimbu@Dikshya-MacBook-Air.local>
2024-10-19 16:12:41 +1100

    remove dynamic port binding

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,6 @@
-import os
 import logging
 from flask import Flask, request
-from app.routes.vqa_routes import vqa_routes
+from backend.app.routes.vqa_routes import vqa_routes
 from flask_cors import CORS
 
 
@@ -38,9 +37,5 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
 
-    # Dynamically bind to the port provided by Render (or default to 5000 for local dev)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+app = create_app()
